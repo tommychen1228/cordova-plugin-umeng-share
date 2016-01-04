3be483ddee80e7d05981dd9ab54cd888ad5f8b47
Tommy Chen <mailcdm@gmail.com>
2016-01-05 02:11:58 +0800

    修改实现方式

--- www/umeng-share.js
+++ www/umeng-share.js
@@ -1,9 +1,8 @@
-UMengShare = {};
-
-UMengShare.init = function(successHandler, errorHandler){
-	cordova.exec(successHandler, errorHandler, "UMengSharePlugin", "init", []);
-};
-
-UMengShare.share:function(text, title, url, successHandler, errorHandler){
-	cordova.exec(successHandler, errorHandler, "UMengSharePlugin", "share", [text, title, url]);
+module.exports = {
+    init: function (successHandler, errorHandler) {
+        cordova.exec(successHandler, errorHandler, "UMengSharePlugin", "init", []);
+    },
+    share:function(text, title, url, successHandler, errorHandler){
+		cordova.exec(successHandler, errorHandler, "UMengSharePlugin", "share", [text, title, url]);
+	}
 };
