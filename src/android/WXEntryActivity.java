542542366d5656f065a71ffbbbf514e02341c3f8
Tommy Chen <mailcdm@gmail.com>
2016-01-05 00:09:50 +0800

    初始化

---
+++ src/android/WXEntryActivity.java
@@ -0,0 +1,9 @@
+package com.myideaway.cordova.umeng.share.WXEntryActivity;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * Created by ntop on 15/9/4.
+ */
+public class WXEntryActivity extends WXCallbackActivity {
+}
